aaa0df2a5b90d6cf065dd078e2a726e422cfa351
Henry Choi <hgstyle@gmail.com>
2022-06-09 22:25:35 +0900

    06092022 night

--- 1016.cpp
+++ 1016.cpp
@@ -6,40 +6,98 @@
 // Question 1016, BJ Online Judge
 // https://www.acmicpc.net/problem/1016
 
-#include <iostream>
-#include <cmath>
+#include<iostream>
+#include<cmath>
+#include<vector>
 using namespace std;
-
-bool is_not_pow(int n) {
-	unsigned long long tmp = 0;
-
-	for (unsigned long long cnt = 2; cnt < unsigned long long(sqrt(n)) + 1; cnt++) {
-		if (n % (cnt * cnt) == 0) {
-			tmp++;
-			break;
-		}
-	}
-
-	if (tmp == 0) {
-		return true;
-	}
-
-	return false;
-}
 
 int main() {
 	ios_base::sync_with_stdio(false);
 	cin.tie(NULL); cout.tie(NULL);
 
-	unsigned long long m1, m2, cnt = 0;
-	
-	cin >> m1 >> m2;
+	long long min_value, max_value, sth_integer = 0, powered_number = 0, cnt = 0;
+	vector<bool> none_sqrt_numbers(1000001, false);
 
-	for (unsigned long long i = m1; i <= m2; ++i) {
-		cnt += is_not_pow(i);
+	cin >> min_value >> max_value;
+
+	long long last_sqrted_number = ceil(sqrt(double(max_value)));
+	for (long long i = 2; i <= last_sqrted_number; ++i) {
+		powered_number = (long long)pow(i, 2);
+		sth_integer = ceil(double(min_value) / double(powered_number)) * powered_number;
+
+		if (sth_integer > max_value) { // checking an useless loop
+			continue;
+		}
+
+		for (sth_integer; sth_integer <= max_value; sth_integer += powered_number) {
+			none_sqrt_numbers[sth_integer - min_value] = true; // true means 'can be divided by powered number'
+		}
+	}
+
+	for (long long i = 0; i <= max_value - min_value; i++) {
+		if (!none_sqrt_numbers[i]) {
+			cnt++;
+		}
 	}
 
 	cout << cnt;
 
 	return 0;
 } // END
+
+
+// Type 2. overtime case
+/*
+#include <iostream>
+#include <cmath>
+#include <set>
+using namespace std;
+
+bool is_prime(long long n) {
+	long long j = sqrt(n);
+
+	for (long long i = 2; i < j + 1; i++) {
+		if (n % i == 0) {
+			return false;
+			break;
+		}
+	}
+
+	return true;
+}
+
+int main() {
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL); cout.tie(NULL);
+
+	long long min_value, max_value, tmp;
+	int main_cnt = 0, sub_cnt = 0;
+	set<long long> numbers;
+
+	cin >> min_value >> max_value;
+
+	for (long long i = 2; pow(i, 2) < max_value; ++i) {
+		if (is_prime(i)) {
+			numbers.insert(pow(i, 2));
+		}
+	}
+
+	for (long long i = min_value; i <= max_value + 1; ++i) {
+		sub_cnt = 0;
+		
+		for (set<long long>::iterator iter = numbers.begin(); iter != numbers.end(); ++iter) {
+			if (i % *iter == 0) {
+				sub_cnt++; 
+			}
+		}
+		if (sub_cnt == 0) {
+			main_cnt++;
+		}
+		
+	}
+
+	cout << main_cnt;
+
+	return 0;
+} // END
+*/
