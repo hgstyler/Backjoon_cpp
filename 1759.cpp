743b63955105a05cf8e413a4e75c6739bb59874b
Henry Choi <hgstyle@gmail.com>
2022-06-12 21:48:07 +0900

    06122022

--- 1759.cpp
+++ 1759.cpp
@@ -11,32 +11,66 @@
 #include<algorithm>
 using namespace std;
 
-void gen_passwd(int &leng, int &cha, vector<int> &v) {
-	
+int l, c;
+vector<char> vec;
+vector<bool> is_pw(15);
+
+void gen(int jj, string s) {
+	if (jj == l) {
+		int cons = 0;
+		int vows = 0;
+
+		for (int i = 0; i < l; ++i) {
+			if (s[i] == 'a' || s[i] == 'e' || s[i] == 'i' || s[i] == 'o' || s[i] == 'u') {
+				vows++;
+				continue;
+			}
+			else {
+				cons++;
+			}
+		}
+
+		if (cons >= 2 && vows >= 1) {
+			cout << s << "\n";
+		}
+		return;
+	}
+
+	for (int i = 0; i < c; ++i) {
+		if (!is_pw[i]) {
+			if (!s.empty()) {
+				if (s.back() > vec[i]) {
+					continue;
+				}
+			}
+
+			is_pw[i] = true;
+
+			s.push_back(vec[i]);
+			gen(jj + 1, s);
+
+			s.pop_back();
+			is_pw[i] = false;
+		}
+	}
 }
 
 int main() {
 	ios_base::sync_with_stdio(false);
 	cin.tie(NULL); cout.tie(NULL);
 
-	int l, c;
 	cin >> l >> c;
-	vector<char> samples(c);
-	vector<char> passwd;
-
 	for (int i = 0; i < c; ++i) {
-		cin >> samples[i];
-	}
-	sort(samples.begin(), samples.end());
-
-	for (int i = 0; i < c; ++i) {
-		cout << samples[i] << " ";
+		char c;
+		cin >> c;
+		vec.push_back(c);
 	}
 
-	for (int i = 0; i < l; ++i) {
-		passwd.push_back(samples[i]);
+	sort(vec.begin(), vec.end());
 
-	}
+	gen(0, "");
 
 	return 0;
-} // END
+}
+
+// END
