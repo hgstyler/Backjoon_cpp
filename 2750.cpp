5d996f3a233e34753c68905a9f744cb31548abfa
Henry Choi <hgstyle@gmail.com>
2022-06-26 19:16:03 +0900

    06262022

---
+++ 2750.cpp
@@ -0,0 +1,31 @@
+// * ** * ** *** ** * ** *
+// BJ Online Judge Project
+// by hgstyler based on C++
+// * ** * ** *** ** * ** *
+
+// Question 2750, BJ Online Judge
+// https://www.acmicpc.net/problem/2750
+
+#include <iostream>
+#include <set>
+using namespace std;
+
+int main() {
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL); cout.tie(NULL);
+
+	int n, tmp;
+	cin >> n;
+	set<int> numbers;
+
+	for (int i = 0; i < n; ++i) {
+		cin >> tmp;
+		numbers.insert(tmp);
+	}
+
+	for (set<int>::iterator iter = numbers.begin(); iter != numbers.end(); ++iter) {
+		cout << *iter << "\n";
+	}
+
+	return 0;
+} // END
